596be07b219950807d9ced29d3dd44b751417372
hargar7 <72989015+hargar7@users.noreply.github.com>
2021-05-03 20:47:09 -0400

    update

---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,12 @@
+// Create the addNumbers function here.
+var two = 2;
+var three = 3;
+var five = two + three;
+
+console.log( five);
+// Create the stringConcat function here.
+var taco = "taco"
+var cat = "cat"
+var tacoCat = taco + cat;
+
+alert(tacoCat);
